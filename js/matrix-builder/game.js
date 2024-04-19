253dcc26a7763d9ebc0e281e28854f5dc604d859
Andrew Salls <asalls2@wpi.edu>
2024-04-19 02:02:08 -0400

    Game polish + mobile

--- js/matrix-builder/game.js
+++ js/matrix-builder/game.js
@@ -1,23 +1,57 @@
-import { generateMatrixShell } from "./generate-matrix.js";
+import { generateMatrixShell, showPopup } from "./generate-matrix.js";
 import fillMatrix from "./fill-matrix.js";
 import getSortedLevels from "./level.js";
 
-function loadLevelData(level) {
+const count = 200;
+const defaults = {
+    origin: { y: 0.7 }
+};
+
+function fire(particleRatio, opts) {
+    confetti({
+        ...defaults,
+        ...opts,
+        particleCount: Math.floor(count * particleRatio)
+    });
+}
+
+async function loadLevelData(level) {
     const rows = Math.pow(2, level.m);
     const columns = Math.pow(2, level.n);
 
-    document.getElementById("goal").src = level.goal;
+    const goalHTML = document.querySelector("#goal");
+    goalHTML.innerHTML = await fetch(level.goal).then(r => r.text());
     const optimalSolution = level.optimalSolution;
 
     // set checkboxes
-    document.getElementById("commute").checked = level.allowedProperties.commute;
-    document.getElementById("complement").checked = level.allowedProperties.complement;
-    document.getElementById("reverse").checked = level.allowedProperties.reverse;
-    document.getElementById("slice-prefix").checked = level.allowedProperties.slicePrefix;
-    document.getElementById("slice-suffix").checked = level.allowedProperties.sliceSuffix;
-    document.getElementById("slice-circumfix").checked = level.allowedProperties.sliceCircumfix;
+    document.querySelector("#commute").checked = level.allowedProperties.commute;
+    document.querySelector("#complement").checked = level.allowedProperties.complement;
+    document.querySelector("#reverse").checked = level.allowedProperties.reverse;
+    document.querySelector("#slice-prefix").checked = level.allowedProperties.slicePrefix;
+    document.querySelector("#slice-suffix").checked = level.allowedProperties.sliceSuffix;
+    document.querySelector("#slice-circumfix").checked = level.allowedProperties.sliceCircumfix;
 
     generateMatrixShell(rows, columns);
+
+    const matrix = document.querySelector("#table");
+    const goalSVG = goalHTML.querySelector("svg");
+    const matrixWidth = matrix.getAttributeNS(null, "width");
+    const matrixHeight = matrix.getAttributeNS(null, "height");
+    goalSVG.setAttributeNS(null, "width", matrixWidth);
+    goalSVG.setAttributeNS(null, "height", matrixHeight);
+    const svgRows = goalSVG.querySelectorAll("g");
+    for (let row = 0; row < svgRows.length; row++) {
+        const rowCols = svgRows[row].querySelectorAll("rect");
+        for (let col = 0; col < rowCols.length; col++) {
+            rowCols[col].setAttributeNS(null, "x", col * matrixHeight / rows);
+            rowCols[col].setAttributeNS(null, "y", row * matrixWidth / columns);
+            rowCols[col].setAttributeNS(null, "width", matrixWidth / rows);
+            rowCols[col].setAttributeNS(null, "height", matrixHeight / columns);
+
+            const fill = rowCols[col].getAttributeNS(null, "fill");
+            rowCols[col].dataset.length = (fill === "white" || fill === "#FFFFFF") ? "Unknown" : "Known";
+        }
+    }
 
     document.querySelector("#fill-matrix").onclick = () => checkSolution(rows, columns, optimalSolution);
     document.querySelector("#clear-matrix").onclick = () => generateMatrixShell(rows, columns);
@@ -26,26 +60,88 @@ function loadLevelData(level) {
 function checkSolution(rows, columns, optimalSolution) {
     fillMatrix(rows, columns);
 
-    // current checking method does not work
-    // maybe compare the rect colors instead?
+    const matrix = document.querySelector("#table");
+    const matrixEntries = matrix.querySelectorAll("rect");
 
-    var solutionSVGData = table.innerHTML;
-    fetch(document.getElementById("goal").src)
-        .then(response => response.text())
-        .then(text => {
-            const parsed = new DOMParser().parseFromString(text, 'text/html');
-            var goalSVGData = parsed.querySelector('svg').innerHTML;
-            if (solutionSVGData == goalSVGData) {
-                if (currentSolution == optimalSolution) {
-                    alert("You have found an optimal solution!");
-                } else {
-                    alert("You have found a solution, but it is not optimal.");
-                }
-            } else {
-                alert("Your solution is incorrect. Try again!");
-                generateMatrixShell(rows, columns);
+    const goalHTML = document.querySelector("#goal");
+    const goalSVG = goalHTML.querySelector("svg");
+    const svgRows = goalSVG.querySelectorAll("g");
+    let count = 0;
+
+    for (let row = 0; row < svgRows.length; row++) {
+        const rowCols = svgRows[svgRows.length - row - 1].querySelectorAll("rect");
+        for (let col = 0; col < rowCols.length; col++) {
+            const tableUnknown = matrix.querySelector(`rect[data-x='${col}'][data-y='${row}']`).dataset.length === "Unknown";
+            const goalUnknown = rowCols[col].dataset.length === "Unknown";
+            if (tableUnknown !== goalUnknown) {
+                displayResult("incorrect");
+                return;
             }
+
+            if (matrixEntries[row * rowCols.length + col].dataset.derivation === "User Selected") {
+                count += 1;
+            }
+        }
+    }
+
+    if (count <= optimalSolution) {
+        displayResult("optimal", optimalSolution);
+    } else {
+        displayResult("suboptimal", optimalSolution);
+        generateMatrixShell(rows, columns);
+    }
+}
+
+function displayResult(type, optimal) {
+    switch (type) {
+        case "incorrect":
+            document.querySelector("#result-heading").textContent = "Not Quite...";
+            document.querySelector("#result-body").textContent = "Click/Tap anywhere to try again";
+            break;
+        case "optimal":
+            document.querySelector("#result-heading").textContent = "Great Job!";
+            document.querySelector("#result-body").innerHTML = `You reached our goal score of ${optimal} squares!.<br />Click/Tap anywhere to close`;
+            break;
+        case "suboptimal":
+            document.querySelector("#result-heading").textContent = "Good, but...";
+            document.querySelector("#result-body").innerHTML = `You can do better! The best solution has at most ${optimal} squares selected.<br />Click/tap anywhere to try again`;
+            break;
+        default:
+            console.error("Unexpected result type provided.");
+    }
+    const resultPopup = document.querySelector("#result-popup");
+    resultPopup.onclick = null;
+    resultPopup.classList.remove("hidden");
+
+    if (type === "optimal") {
+        fire(0.25, {
+            spread: 26,
+            startVelocity: 55,
         });
+        fire(0.2, {
+            spread: 60,
+        });
+        fire(0.35, {
+            spread: 100,
+            decay: 0.91,
+            scalar: 0.8
+        });
+        fire(0.1, {
+            spread: 120,
+            startVelocity: 25,
+            decay: 0.92,
+            scalar: 1.2
+        });
+        fire(0.1, {
+            spread: 120,
+            startVelocity: 45,
+        });
+    }
+
+    // Prevent accidentally closing
+    setTimeout(() => {
+        resultPopup.onclick = () => resultPopup.classList.add("hidden");
+    }, 200);
 }
 
 window.addEventListener("DOMContentLoaded", async () => {
@@ -59,5 +155,54 @@ window.addEventListener("DOMContentLoaded", async () => {
     document.querySelector("#game-label").textContent = `Level ${levelNum + 1}`;
 
     const levels = await getSortedLevels();
-    loadLevelData(levels[parseInt(levelNum)]);
+    const level = levels[parseInt(levelNum)];
+
+    window.onresize = () => {
+        const rows = Math.pow(2, level.m);
+        const columns = Math.pow(2, level.n);
+
+        var cellWidth = -6.75 * level.m + 51.75;
+        var cellHeight = -6.75 * level.n + 51.75;
+        const cellSize = Math.min(cellWidth, cellHeight);
+
+        const tableMatrix = document.querySelector("#table");
+        tableMatrix.setAttributeNS(null, "width", columns * cellWidth);
+        tableMatrix.setAttributeNS(null, "height", rows * cellHeight);
+
+        tableMatrix.querySelectorAll("rect").forEach(e => {
+            e.setAttributeNS(null, "x", cellSize * e.dataset.x);
+            e.setAttributeNS(null, "y", cellSize * (rows - e.dataset.y - 1)); // reverses so that (00..., 00...) is in bottom left instead of top left
+            e.setAttributeNS(null, "width", cellSize);
+            e.setAttributeNS(null, "height", cellSize);
+        });
+
+        const goalHTML = document.querySelector("#goal");
+        const goalSVG = goalHTML.querySelector("svg");
+        const matrixWidth = tableMatrix.getAttributeNS(null, "width");
+        const matrixHeight = tableMatrix.getAttributeNS(null, "height");
+        goalSVG.setAttributeNS(null, "width", matrixWidth);
+        goalSVG.setAttributeNS(null, "height", matrixHeight);
+        const svgRows = goalSVG.querySelectorAll("g");
+        for (let row = 0; row < svgRows.length; row++) {
+            const rowCols = svgRows[row].querySelectorAll("rect");
+            for (let col = 0; col < rowCols.length; col++) {
+                rowCols[col].setAttributeNS(null, "x", col * matrixHeight / rows);
+                rowCols[col].setAttributeNS(null, "y", row * matrixWidth / columns);
+                rowCols[col].setAttributeNS(null, "width", matrixWidth / rows);
+                rowCols[col].setAttributeNS(null, "height", matrixHeight / columns);
+
+                const fill = rowCols[col].getAttributeNS(null, "fill");
+                rowCols[col].dataset.length = (fill === "white" || fill === "#FFFFFF") ? "Unknown" : "Known";
+            }
+        }
+    };
+
+    const popup = document.querySelector("#info-popup");
+    document.querySelector("main").onscroll = () => {
+        if (!popup.classList.contains("hidden")) {
+            showPopup();
+        }
+    };
+
+    loadLevelData(level);
 });
